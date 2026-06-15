6fb0470d623514e3796dde4abfb601fc59cdb2e9
WarVad <vasik1596@gmail.com>
2026-06-15 19:08:47 +0200

    Přidejte soubory projektu.

---
+++ Auta/Models/Kategorie.cs
@@ -0,0 +1,42 @@
+﻿namespace Auta.Models
+{
+    public class Kategorie
+    { 
+        private string nazevKategorie = string.Empty;
+        private bool jedenVyber = false;
+        private bool bylUzJedenVybran = false;
+        private List<Komponenty> komponenty = new List<Komponenty>();
+
+        
+        
+        
+
+
+        public string NazevKategorie
+        {
+            get { return nazevKategorie; }
+            set { nazevKategorie = value; }
+        }
+
+        public bool JedenVyber
+        {
+            get { return jedenVyber; }
+            set { jedenVyber = value; }
+        }
+
+        public List<Komponenty> Komponenty
+        {
+            get { return komponenty; }
+            set { komponenty = value; }
+        }
+
+        public bool BylUzJedenVybran
+        {
+            get { return bylUzJedenVybran; }
+            set { bylUzJedenVybran = value; }
+        }
+
+
+
+    }
+}
---
+++ Auta/Models/Komponenty.cs
@@ -0,0 +1,53 @@
+﻿namespace Auta.Models
+{
+    public class Komponenty
+    {
+        private string nazev = string.Empty;
+        private string popis = string.Empty;
+        private bool zakoupeno = false;
+        private int cena = 0;
+
+
+
+
+        public string Nazev
+        {
+            get { return nazev; }
+            set { nazev = value; }
+        }
+
+        public string Popis
+        {
+            get { return popis; }
+            set { popis = value; }
+        }
+
+        public bool Zakoupeno
+        {
+            get { return zakoupeno; }
+            set { zakoupeno = value; }
+        }
+        public int Cena
+        {
+            get { return cena; }
+            set
+            {
+                if (value < 0)
+                {
+                    cena = 0;
+                }
+                else
+                {
+                    cena = value;
+                }
+            }
+        }
+
+
+        
+
+    }
+}
+
+        
+    
